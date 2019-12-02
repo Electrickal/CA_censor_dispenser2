4c3a78a59d9a5dc75bcc1e1b55a1d4e408b36795
Electrickal <erickjm.eng@gmail.com>
2019-12-01 22:36:04 -0800

    Added a new function that censors subsequent mentions of a "negative word" after 2 mentions

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -8,6 +8,8 @@ email_four = open("email_four.txt", "r").read()
 ###				Variables 				###
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
+
 
 ### 			Functions 				###
 
@@ -20,9 +22,32 @@ def censor_email(email, prop_terms):
 
 	return email
 
+# Removes subsequent mentions of "negative words" from a text strig after one mention of a word.
+def negative_remove(email, neg_terms):
+	for term in neg_terms:
 
-print(censor_email(email_two, proprietary_terms))
-print("#" * len(proprietary_terms[0]))
+		# Use word_index to find the 3rd mention of term in a string. start index at -1
+		word_index = -1
+		for i in range(2):
+			word_index = email.lower().find(term[word_index + 1 :])
+
+		if word_index > 0:
+			email = email.replace(term + ' ', '#' * len(term) + ' ')
+			email = email.replace(term.title() + ' ', '#' * len(term) + ' ')
+			email = email.replace(term.upper() + ' ', '#' * len(term) + ' ')
+
+	return email
+def censor_neg_remove_email(email, neg_terms, prop_terms):
+	email = negative_remove(email, neg_terms)
+	email = censor_email(email, prop_terms)
+
+	return email
+
+
+print(email_three)
+print(censor_neg_remove_email(email_three, negative_words, proprietary_terms))
+
+
 
 
 
